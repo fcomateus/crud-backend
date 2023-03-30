43b9686ef2b288bb7033f955a9e07b0fc0cfbcfe
mateus <fmfr@outlook.com>
2023-03-30 16:24:26 -0300

    added useful comments

--- src/PersonController.js
+++ src/PersonController.js
@@ -4,7 +4,6 @@ class PersonController {
     async create(req, res) {
         const { nome, dt_nasc, idade, cep, rua, numero, bairro } = req.body;
 
-        console.log(cep);
         //retornar o id do usuário inserido
         const [pessoa] = await knex("pessoas")
         .returning("id")
@@ -14,7 +13,6 @@ class PersonController {
             dt_nasc
         })
 
-        console.log("pessoa",pessoa);
 
         await knex("enderecos").insert({
             id_pessoa: pessoa.id,
@@ -24,13 +22,14 @@ class PersonController {
             bairro
         })
 
-        console.log("Inserido");
         return res.status(200).json("Inserido")
     }
 
     async show(req, res) {
         const { id } = req.params;
 
+        //o knex retorna um array de objetos. Dessa forma,
+        //cada objeto será uma posição do array "dados"
         const [dados] = await knex
         .select([
             "pessoas.id",
@@ -45,22 +44,7 @@ class PersonController {
         .from("pessoas")
         .where("pessoas.id", id)
         .innerJoin("enderecos", "pessoas.id", "enderecos.id_pessoa")
-
-        console.log(dados);
-        // const mock = 
-        //     {
-        //         id: 1,
-        //         nome:"João da Silva",
-        //         idade:"32",
-        //         dt_nasc:"20/06/1991",
-        //         cep:"999999999",
-        //         rua:"Rua das Flores",
-        //         numero:"00",
-        //         bairro:"Edson Queiroz"
-        //     };
         
-        
-        console.log(dados);
         return res.json(dados);
     }
 
@@ -71,19 +55,17 @@ class PersonController {
         await knex("pessoas").update({nome, dt_nasc, idade}).where({id});
         await knex("enderecos").update({cep, rua, numero, bairro}).where({id_pessoa: id});
         
-        console.log("Atualizado");
         return res.status(200).json("Atualizado")
     }
 
     async delete(req, res) {
         const { id } = req.params;
 
+        // deleção ON CASCADE faz com que na tabela "enderecos"
+        // o registro associado seja deletado junto
+
         await knex("pessoas").where({id}).delete();
         
-        //await knex("enderecos").where({id_pessoa:id}).delete();  
-        
-        console.log(`ID ${id} deletado`);
-
         return res.json("Deletado");
     }
 
@@ -108,41 +90,7 @@ class PersonController {
         .innerJoin("enderecos", "pessoas.id", "enderecos.id_pessoa")
         .orderBy("pessoas.nome");
 
-        // const mock = [
-        //     {
-        //         id: 1,
-        //         nome:"João da Silva",
-        //         idade:"32",
-        //         dt_nasc:"20/06/1991",
-        //         cep:"999999999",
-        //         rua:"Rua das Flores",
-        //         numeror:"00",
-        //         bairro:"Edson Queiroz"
-        //     },
-        //     {
-        //         id: 2,
-        //         nome:"Joaquina Pereira",
-        //         idade:"20",
-        //         dt_nasc:"15/10/2002",
-        //         cep:"111111111",
-        //         rua:"Rua das Folhas",
-        //         numero:"01",
-        //         bairro:"Jardim das Oliveiras"
-        //     },{
-        //         id: 3,
-        //         nome:"Antonia Pereira",
-        //         idade:"50",
-        //         dt_nasc:"27/30/1967",
-        //         cep:"111111111",
-        //         rua:"Rua das Folhas",
-        //         numero:"01",
-        //         bairro:"Jardim das Oliveiras"
-        //     },
-        // ];
-
-        console.log(pessoas);
         return res.json(pessoas);
-
     }
 
 }
